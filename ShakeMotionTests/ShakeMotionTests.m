1926d7762624c70e8924512682664ed29915c819
vikas <vikas@vikass-iMac.local>
2017-10-05 11:32:59 +0530

    Initial Commit

---
+++ ShakeMotionTests/ShakeMotionTests.m
@@ -0,0 +1,39 @@
+//
+//  ShakeMotionTests.m
+//  ShakeMotionTests
+//
+//  Created by vikas on 05/10/17.
+//  Copyright © 2017 vikas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShakeMotionTests : XCTestCase
+
+@end
+
+@implementation ShakeMotionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
