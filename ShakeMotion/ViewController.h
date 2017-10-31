2fe2079c58bc013b3d8c062ecbf9f6a3f8cbb1a8
vjadhav986 <vjadhav986@gmail.com>
2017-10-31 14:28:13 +0530

    test
    test

--- ShakeMotion/ViewController.h
+++ ShakeMotion/ViewController.h
@@ -7,9 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
+
+double currentMaxAccelX;
+double currentMaxAccelY;
+double currentMaxAccelZ;
+double currentMaxRotX;
+double currentMaxRotY;
+double currentMaxRotZ;
 
 @interface ViewController : UIViewController
 
+- (IBAction)resetAction:(id)sender;
 
+@property (strong, nonatomic) CMMotionManager *motionManager;
+@property (weak, nonatomic) IBOutlet UIImageView *fruit1Img;
+@property (weak, nonatomic) IBOutlet UIImageView *fruit2Img;
+@property (weak, nonatomic) IBOutlet UIImageView *fruit3Img;
+@property (weak, nonatomic) IBOutlet UIImageView *fruit4Img;
+@property (weak, nonatomic) IBOutlet UIImageView *fruit5Img;
+@property (weak, nonatomic) IBOutlet UIImageView *fruit6Img;
+@property (weak, nonatomic) IBOutlet UILabel *animLbl;
 @end
 
--- ShakeMotion/ViewController.m
+++ ShakeMotion/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIView+RZViewActions.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    currentMaxAccelX = 0;
+    currentMaxAccelY = 0;
+    currentMaxAccelZ = 0;
+    
+    currentMaxRotX = 0;
+    currentMaxRotY = 0;
+    currentMaxRotZ = 0;
+    
+    [self animation1];
+    
+}
+
+-(BOOL)canBecomeFirstResponder {
+    return YES;
+}
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self becomeFirstResponder];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [self resignFirstResponder];
+    [super viewWillDisappear:animated];
+}
+
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    
+    if (motion == UIEventSubtypeMotionShake)
+    {
+        // your code
+        NSLog(@"Detected  motionBegan ..........");
+        
+        [UIView animateWithDuration:2.0 delay:0 options:UIViewAnimationOptionCurveLinear  animations:^{
+            //code with animation
+            
+            self.fruit1Img.frame = CGRectMake(355, 135, 132, 114);
+            self.fruit2Img.frame = CGRectMake(220, 478, 261, 282);
+            self.fruit3Img.frame = CGRectMake(557, 254, 227, 282);
+            self.fruit4Img.frame = CGRectMake(255, 368, 49, 62);
+            self.fruit5Img.frame = CGRectMake(344, 399, 62, 71);
+            self.fruit6Img.frame = CGRectMake(457, 257, 78, 76);
+            
+        } completion:^(BOOL finished) {
+            //code for completion
+            NSLog(@"code completed");
+        }];
+    }
+}
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (motion == UIEventSubtypeMotionShake)
+    {
+        // your code
+        NSLog(@"Detected motionEnded ..........");
+    }
 }
 
 
@@ -26,4 +83,41 @@
 }
 
 
+- (IBAction)resetAction:(id)sender {
+    
+    [UIView animateWithDuration:2.0 delay:0 options:UIViewAnimationOptionCurveLinear  animations:^{
+        //code with animation
+        self.fruit1Img.frame = CGRectMake(314, 53, 40, 40);
+        self.fruit2Img.frame = CGRectMake(385, 53, 40, 40);
+        self.fruit3Img.frame = CGRectMake(460, 53, 40, 40);
+        self.fruit4Img.frame = CGRectMake(530, 53, 40, 40);
+        self.fruit5Img.frame = CGRectMake(598, 53, 40, 40);
+        self.fruit6Img.frame = CGRectMake(670, 53, 40, 40);
+        
+    } completion:^(BOOL finished) {
+        //code for completion
+        NSLog(@"code completed");
+    }];
+
+}
+
+-(void)animation1{
+    
+    RZViewAction *scaleUp = [RZViewAction action:^{
+        self.animLbl.transform = CGAffineTransformScale(self.animLbl.transform, 1.3f, 1.3f);
+    } withOptions:UIViewAnimationOptionCurveEaseOut duration:0.2];
+    
+    RZViewAction *scaleDown = [RZViewAction action:^{
+        self.animLbl.transform = CGAffineTransformIdentity;
+    } withDuration:0.15];
+    
+    RZViewAction *wait = [RZViewAction waitForDuration:0.7];
+    
+    RZViewAction *pulse = [RZViewAction sequence:@[scaleUp, scaleDown]];
+    RZViewAction *heartbeat = [RZViewAction sequence:@[pulse, wait, pulse, wait, pulse]];
+    
+    [UIView rz_runAction:heartbeat withCompletion:^(BOOL finished) {
+        NSLog(@"Animation complete!");
+    }];
+}
 @end
